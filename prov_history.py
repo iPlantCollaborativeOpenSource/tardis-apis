283201f0b44b8fe0c69a2fb365b482d54d93b809
Andrew Lenards <lenards@iplantcollaborative.org>
2013-02-20 20:37:03 -0700

    PEP-8, pylint changes - add private method; score 8.12/10

--- prov_history.py
+++ prov_history.py
@@ -40,142 +40,188 @@
 #
 #
 # Author: Sangeeta Kuchimanchi (sangeeta@iplantcollaborative.org)
-# Date: 10/11/2012 
+# Date: 10/11/2012
 #
 
-import os
-import string
 import sys
-import datetime
-import time
-import smtplib
 import MySQLdb
-from email.MIMEText import MIMEText
-import site
 
 CONFIG_PATH = '/scripts'
 
 sys.path.append(CONFIG_PATH)
-from db_queries import *
-from configs import *
-from scriptTracking import *
 
+from db_queries import (PROV_HIST_SELECT_ID, PROV_HIST_INSERT_ID,
+                        CHILD_HIST_SELECT_QUERY, PARENT_HIST_SELECT_QUERY,
+                        PROV_HIST_UPDATE_STATUS)
+from configs import (PROV_DB_HOST, PROV_DB_NAME, PROV_DB_USERNAME,
+                    PROV_DB_PASSWORD)
+from script_tracking import (notifySupport, trackHistoryErrors,
+                            trackHistoryExceptions, trackHistoryInfo)
 
-def insertHistory():
- 
-  scriptname = "History"
+# might as well define this as a global
+SCRIPTNAME = "History"
 
-  try:
- 
-    conn = MySQLdb.connect (host = PROV_DB_HOST,user = PROV_DB_USERNAME,passwd = PROV_DB_PASSWORD,db = PROV_DB_NAME)
-    cursor = conn.cursor ()
-  
-  except:
-    
-    errMsg = "Connection failed to Provenance database."
-    trackHistoryExceptions(errMsg)
-    notifySupport(errMsg,scriptname)
-       
-  try:
+def _process_single_child_results(cursor, parent_id, parent_history_code,
+                                    parent_id_results):
+    """Private method for processing child results for parent records
+    that only have a single result. """
+    p_id = parent_id
+    p_history_code = parent_history_code
+    p_id_results = parent_id_results
+    p_provenance_id = int(p_id_results[0][0])
+    child_hist = cursor.execute(CHILD_HIST_SELECT_QUERY %
+                                (p_history_code, 'N', 'N'))
+    c_results = cursor.fetchall()
 
-    cursor.execute(PARENT_HIST_SELECT_QUERY % ('Y','N'))
-    p_results = cursor.fetchall()
- 
+    for child in c_results:
+        c_id = int(child[0])
+        c_uuid = int(child[2])
+        c_service_id = int(child[3])
+        c_category_id = int(child[4])
+        c_event_id = int(child[5])
+        c_username = str(child[6])
+        c_createdate = int(child[7])
 
-    if len(p_results) != 0:
-      
-      for parent in p_results:
+        c_histid_status = cursor.execute(PROV_HIST_SELECT_ID %
+                                        (c_uuid, c_event_id,
+                                        c_category_id,
+                                        c_service_id, c_username,
+                                        c_createdate))
+        c_id_results = cursor.fetchall()
 
-        p_id = int(parent[0])
-        p_history_code = str(parent[1])
-        p_uuid = int(parent[2])
-        p_service_id = int(parent[3])
-        p_category_id = int(parent[4])
-        p_event_id = int(parent[5])
-        p_username = str(parent[6])
-        p_createdate = int(parent[7])
-
-        proc_return = cursor.execute(PROV_HIST_SELECT_ID % (p_uuid,p_event_id,p_category_id,p_service_id,p_username,p_createdate))
-        p_id_results = cursor.fetchall()
- 
-
-        if len(p_id_results) == 1:
-          p_provenance_id = int(p_id_results[0][0])
-
-          child_hist = cursor.execute(CHILD_HIST_SELECT_QUERY % (p_history_code,'N','N'))
-          c_results = cursor.fetchall()
-
-          for child in c_results:
-            c_id = int(child[0])
-            c_uuid = int(child[2])
-            c_service_id = int(child[3])
-	    c_category_id = int(child[4])
- 	    c_event_id = int(child[5])
-	    c_username = str(child[6])
-	    c_createdate = int(child[7])
-
-            c_histid_status = cursor.execute(PROV_HIST_SELECT_ID % (c_uuid,c_event_id,c_category_id,c_service_id,c_username,c_createdate))
-            c_id_results = cursor.fetchall()
- 
-            if len(c_id_results) == 1:
-               c_provenance_id = int(c_id_results[0][0])
-
-               c_histid_status = cursor.execute(PROV_HIST_INSERT_ID % (c_provenance_id,p_provenance_id))
-               if c_histid_status == 1:
-                 c_process_status = cursor.execute(PROV_HIST_UPDATE_STATUS % ('Y',c_id))
-                 if c_process_status == 1:
-                   infoMsg = "Child row updated: " + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]" 
-                   trackHistoryInfo(infoMsg)
-                 else:
-                   errMsg = "Failed to update child row status:" + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-                   trackHistoryErrors(errMsg)
-                   notifySupport(errMsg,scriptname) 
-               else:
-                 errMsg = "Failed History Recording, at child rown" + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-                 trackHistoryErrors(errMsg)
-                 notifySupport(errMsg,scriptname)
-
+        if len(c_id_results) == 1:
+            c_provenance_id = int(c_id_results[0][0])
+            c_histid_status = cursor.execute(PROV_HIST_INSERT_ID %
+                                            (c_provenance_id,
+                                            p_provenance_id))
+            if c_histid_status == 1:
+                c_process_status = cursor.execute(
+                                        PROV_HIST_UPDATE_STATUS
+                                        % ('Y', c_id))
+                if c_process_status == 1:
+                    info_msg = ("Child row updated: [" +
+                                "parent_provenance_id, " +
+                                "child_provenance_id]:" +
+                                "[" + str(p_id) + "," + str(c_id) +
+                                "]")
+                    trackHistoryInfo(info_msg)
+                else:
+                    err_msg = ("Failed to update child row " +
+                                "status: [parent_provenance_id, " +
+                                "child_provenance_id]:" + "[" +
+                                str(p_id) + "," + str(c_id) + "]")
+                    trackHistoryErrors(err_msg)
+                    notifySupport(err_msg, SCRIPTNAME)
             else:
-               errMsg = "Error in retrieving child row from provenance table, multiple entries or no entry found. " + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-               trackHistoryErrors(errMsg)
-               notifySupport(errMsg,scriptname)
+                err_msg = ("Failed History Recording, at child row"
+                         + " [parent_provenance_id, " +
+                         "child_provenance_id]:" + "[" + str(p_id)
+                         + "," + str(c_id) + "]")
+                trackHistoryErrors(err_msg)
+                notifySupport(err_msg, SCRIPTNAME)
+        else:
+            err_msg = ("Error in retrieving child row from " +
+                        "provenance table, multiple entries or " +
+                        "no entry found. [parent_provenance_id, " +
+                        "child_provenance_id]: [" + str(p_id) +
+                        "," + str(c_id) + "]")
+            trackHistoryErrors(err_msg)
+            notifySupport(err_msg, SCRIPTNAME)
 
-          
-          p_process_status = cursor.execute(PROV_HIST_UPDATE_STATUS % ('Y',p_id))
-          if p_process_status == 1:
-            infoMsg = "History Recorded: " + "[parent_provenance_id]:" + "[" + str(p_id) + "]" 
-            trackHistoryInfo(infoMsg)
-          else:
-            errMsg = "Failed updating parent row status:" + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-            trackHistoryErrors(errMsg)
-            notifySupport(errMsg,scriptname)
-       
-          
-        elif len(p_id_results) > 1:
-          errMsg = "Multiple entries found for parent row. " + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-          trackHistoryExceptions(errMsg)
-          notifySupport(errMsg,scriptname)
-          
-        
-        else: 
-          errMsg = "No entry found for parent row, but history tracking flag enabled." + "[parent_provenance_id,child_provenance_id]:" + "[" + str(p_id) + "," + str(c_id) + "]"
-          trackHistoryExceptions(errMsg)
-          notifySupport(errMsg,scriptname)
+        p_process_status = cursor.execute(PROV_HIST_UPDATE_STATUS %
+                                            ('Y', p_id))
+        if p_process_status == 1:
+            info_msg = ("History Recorded: [parent_provenance_id]:"
+                        + "[" + str(p_id) + "]")
+            trackHistoryInfo(info_msg)
+        else:
+            err_msg = ("Failed updating parent row status:" +
+                        "[parent_provenance_id, " +
+                        "child_provenance_id]:" + "[" + str(p_id) +
+                        "," + str(c_id) + "]")
+            trackHistoryErrors(err_msg)
+            notifySupport(err_msg, SCRIPTNAME)
 
 
-    cursor.close()
+def insert_history():
+    """Handles inserting history records for provenance objects."""
+    try:
+        conn = MySQLdb.connect(host=PROV_DB_HOST, user=PROV_DB_USERNAME,
+                                passwd=PROV_DB_PASSWORD, db=PROV_DB_NAME)
+        cursor = conn.cursor()
+    except:
+        err_msg = "Connection failed to Provenance database."
+        trackHistoryExceptions(err_msg)
+        notifySupport(err_msg, SCRIPTNAME)
 
-  except Exception, e:
+    try:
 
-    errMsg = "Exception occured after establishing DB connection: " + str(e)
-    trackHistoryExceptions(errMsg)
-    notifySupport(errMsg,scriptname)         
-    cursor.close()
+        cursor.execute(PARENT_HIST_SELECT_QUERY % ('Y', 'N'))
+        p_results = cursor.fetchall()
+
+        if len(p_results) != 0:
+
+            for parent in p_results:
+
+                p_id = int(parent[0])
+                p_history_code = str(parent[1])
+                p_uuid = int(parent[2])
+                p_service_id = int(parent[3])
+                p_category_id = int(parent[4])
+                p_event_id = int(parent[5])
+                p_username = str(parent[6])
+                p_createdate = int(parent[7])
+
+                proc_return = cursor.execute(PROV_HIST_SELECT_ID % (p_uuid,
+                                            p_event_id, p_category_id,
+                                            p_service_id, p_username,
+                                            p_createdate))
+                p_id_results = cursor.fetchall()
+
+                # parent ID results contain only 1 result record
+                if len(p_id_results) == 1:
+                    _process_single_child_results(cursor, p_id, p_history_code,
+                                                    p_id_results)
+
+                # parent ID results greater than 1 result record
+                elif len(p_id_results) > 1:
+                    err_msg = ("Multiple entries found for parent row. " +
+                                "[parent_provenance_id,child_provenance_id]:" +
+                                "[" + str(p_id) + "," + str(c_id) + "]")
+                    # NOTE - the above c_id is not available here (undefined)
+                    # need to figure how and why it was needed here...
+                    trackHistoryExceptions(err_msg)
+                    notifySupport(err_msg, SCRIPTNAME)
+
+                else:
+                    err_msg = ("No entry found for parent row, but history " +
+                                "tracking flag enabled. [parent_provenance_id,"
+                                + "child_provenance_id]:" + "[" + str(p_id) +
+                                "," + str(c_id) + "]")
+                    # NOTE - the above c_id is not available here (undefined)
+                    # need to figure how and why it was needed here...
+                    trackHistoryExceptions(err_msg)
+                    notifySupport(err_msg, SCRIPTNAME)
+        # finish up and close our cursor
+        cursor.close()
+
+    except Exception, exc:
+        err_msg = ("Exception occured after establishing DB connection: " +
+                    str(exc))
+        trackHistoryExceptions(err_msg)
+        notifySupport(err_msg, SCRIPTNAME)
+        cursor.close()
+
+
+def main():
+    """Script entry point."""
+    try:
+        insert_history()
+    except:
+        err_msg = "insert_history() was not initialized"
+        notifySupport(err_msg, "History")
+
 
 if __name__ == "__main__":
-	try:
-	  insertHistory()
-	except:
-          errMsg = "insertHistory() was not initialized"
-          notifySupport(errMsg,"History") 
+    main()
 
