e9a8c026f7b865868deb022e069e34659e078b7c
Andrew Lenards <andrew.lenards@gmail.com>
2013-02-27 10:59:06 -0700

    Include scripts meant to run as crontabs

--- crons/prov_history.py
+++ crons/prov_history.py
@@ -1,48 +1,5 @@
 #!/usr/bin/python26
 
-#  Copyright (c) 2011, The Arizona Board of Regents on behalf of
-#  The University of Arizona
-#
-#  All rights reserved.
-#
-#  Developed by: iPlant Collaborative as a collaboration between
-#  participants at BIO5 at The University of Arizona (the primary hosting
-#  institution), Cold Spring Harbor Laboratory, The University of Texas at
-#  Austin, and individual contributors. Find out more at
-#  http://www.iplantcollaborative.org/.
-#
-#  Redistribution and use in source and binary forms, with or without
-#  modification, are permitted provided that the following conditions are
-#  met:
-#
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of the iPlant Collaborative, BIO5, The University
-#   of Arizona, Cold Spring Harbor Laboratory, The University of Texas at
-#   Austin, nor the names of other contributors may be used to endorse or
-#   promote products derived from this software without specific prior
-#   written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-# IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-# PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
-# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-#
-# Author: Sangeeta Kuchimanchi (sangeeta@iplantcollaborative.org)
-# Date: 10/11/2012 
-#
-
 import os
 import string
 import sys
@@ -53,7 +10,7 @@ import MySQLdb
 from email.MIMEText import MIMEText
 import site
 
-CONFIG_PATH = '/scripts'
+CONFIG_PATH = '/scripts/public/v1.3'
 
 sys.path.append(CONFIG_PATH)
 from db_queries import *
