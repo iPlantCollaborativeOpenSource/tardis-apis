81f62d7a0a3c2811ac9a6ce3db2766a2ad635dd6
Andrew Lenards <andrew.lenards@gmail.com>
2013-04-15 08:11:06 -0700

    The WSGI in mod_wsgi is looking for callable named ``application`` - give it what it wants...

--- collector/src/police_box.py
+++ collector/src/police_box.py
@@ -20,7 +20,11 @@ from provenance_agent import application as provenance
 from provenance_post import application as prov_post
 from object_reg_lookup import application as register
 
-__all__ = ['app']
+# Okay code maintainer - I just want to save you some hassle... mod_wsgi
+# is looking for a function or a variable named ``application``. So,
+# guess what - don't change this unless you're running this out of
+# something less like gunicorn/etc.  Oh... You're welcome.
+__all__ = ['application']
 
 def say_hello(environ, start_response):
     """
@@ -31,21 +35,22 @@ def say_hello(environ, start_response):
     start_response("200 OK", [('Content-type', 'text/plain')])
     return ["Hello, %s!" % kwargs['name']]
 
-
-app = Selector()
+# I know - a long variable name, but don't change it - see the above
+# message above ``__all__`` for more context...
+application = Selector()
 
 # include for smoke testing
-app.add('/hello/{name}', GET=say_hello)
-app.add(ENDPT_PREFIX + '/hello/{name}', GET=say_hello)
+application.add('/hello/{name}', GET=say_hello)
+application.add(ENDPT_PREFIX + '/hello/{name}', GET=say_hello)
 
 # Change to include a version number as a prefix
-app.add(ENDPT_PREFIX + '/lookup[/]', GET=lookup)
-app.add(ENDPT_PREFIX +
+application.add(ENDPT_PREFIX + '/lookup[/]', GET=lookup)
+application.add(ENDPT_PREFIX +
     '/register/{object_id:word}/{object_name:segment}/' +
     '{object_desc:segment[/{parent_uuid:word}][/]', POST=register)
-app.add(ENDPT_PREFIX +
+application.add(ENDPT_PREFIX +
     '/provenance/{uuid:word}/{username}/{service_name}/' +
     '{event_name}/{category_name}[/]', POST=provenance)
-app.add(ENDPT_PREFIX +
+application.add(ENDPT_PREFIX +
     '/provenance/{service_name}/{event_name}/{category_name}[/]',
     POST=prov_post)
