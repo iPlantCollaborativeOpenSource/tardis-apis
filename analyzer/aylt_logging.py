bfda059da4bc4479340032ee8c03afc536e3a45d
Andrew Lenards <lenards@iplantcollaborative.org>
2013-04-24 21:25:22 -0700

    First pass for PEP-8 over all files...

--- analyzer/aylt_logging.py
+++ analyzer/aylt_logging.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python26
 
+"""
+A logging utility - nearly identical to the logging utility in Collector.
+"""
+
 import logging
-from config import *
+from configs import ANALYTICS_LOGFILE
 
-logging.basicConfig (level=logging.DEBUG,
-                     format='%(asctime)s %(levelname)-8s %(message)s',
-                     datefmt='%a %Y-%m-%d %H:%M:%S',
-                     filename=ANALYTICS_LOGFILE)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(message)s',
+                    datefmt='%a %Y-%m-%d %H:%M:%S',
+                    filename=ANALYTICS_LOGFILE)
 
 
-def LogException(message):
- 
-  message = str(message)
-  
-  logging.debug(message)
+def log_exception(message):
+    message = str(message)
 
-def LogErrors(message):
-  
-  message = str(message)
+    logging.debug(message)
 
-  logging.error(message)
 
-def LogInfo(message, data):
+def log_errors(message):
+    message = str(message)
 
-  message = str(message)
-  data = str(data)
+    logging.error(message)
 
-  logging.info(message)
-  logging.info(data)
+
+def log_info(message, data):
+    message = str(message)
+    data = str(data)
+
+    logging.info(message)
+    logging.info(data)
