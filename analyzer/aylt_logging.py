a76ffcb83a8ea3c3b238e4763ce1bc3f925e399c
Andrew Lenards <lenards@iplantcollaborative.org>
2013-05-01 21:54:50 -0700

    add docstrings to analyzer logging

--- analyzer/aylt_logging.py
+++ analyzer/aylt_logging.py
@@ -14,18 +14,23 @@ logging.basicConfig(level=logging.DEBUG,
 
 
 def log_exception(message):
+    """Logs exceptions to the configured log file for provenance
+    activities"""
     message = str(message)
-
     logging.debug(message)
 
 
 def log_errors(message):
+    """Logs errors encountered when provenance activities fail.
+    The log file used is the provenance log file defined in
+    `configs.py`."""
     message = str(message)
-
     logging.error(message)
 
 
 def log_info(message, data):
+    """Logs informative messages regarding provenance actitivies to the
+    log file defined in `configs.py`."""
     message = str(message)
     data = str(data)
 
